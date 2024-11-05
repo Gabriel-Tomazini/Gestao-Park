ed3cf1fc3c52bd99d6eefd2e8677a63c5a5f6749
Gabriel-Tomazini <116926456+Gabriel-Tomazini@users.noreply.github.com>
2024-11-04 21:11:23 -0300

    Correção api

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,6 +1,6 @@
 const baseApiUrl =
   process.env.NODE_ENV === "production"
-    ? "estacionamento-rho.vercel.app/api"
+    ? "https://estacionamento-rho.vercel.app/api" // Adicione o "https://" para garantir que a URL esteja completa
     : "http://localhost:3000/api";
 
 export const apiUrls = {
