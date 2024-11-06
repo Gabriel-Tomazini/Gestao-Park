b49525f365dac19a1a4a98ab0ca6ca2c2b51f8a3
Gabriel-Tomazini <116926456+Gabriel-Tomazini@users.noreply.github.com>
2024-11-05 21:10:23 -0300

    Teste Dependências

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,10 @@
 export default {
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implemente os listeners de eventos do nó aqui
     },
+    baseUrl: 'http://localhost:3000', // Defina a URL base do seu aplicativo
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Padrão para os arquivos de teste
+    supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}', // Arquivo de suporte para testes
   },
 };
-
